05ff0c92de02dce4c1a69716ecc30e1b38fd4e4c
diegodubai <dirkt82@hotmail.com>
2025-04-29 15:04:02 +0400

    Update app.js
    Added ⬇️ as a thread indicator

--- app.js
+++ app.js
@@ -206,7 +206,7 @@ function handleTweet(tweet, includes) {
   const convId = tweet.conversation_id;
   const isRoot = convId === tweet.id;
   const text = tweet.note_tweet?.text || tweet.text;
-  const isThreadOpener = /(?:1\/(?:\d+|x)|🧵|\bthread\b|👇)/i.test(text);
+  const isThreadOpener = /(?:1\/(?:\d+|x)|🧵|\bthread\b|⬇️|👇)/i.test(text);
 
   if (threadBuffers.has(convId)) {
     const buf = threadBuffers.get(convId);
