fa520f2d6b8eaac33f074f437936c0bb199555ea
diegodubai <dirkt82@hotmail.com>
2025-05-07 00:33:57 +0400

    Update app.js
    Added to isThreadOpener: 0/, 0/x, 0/number

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ function handleTweet(tweet, includes) {
   const convId = tweet.conversation_id;
   const isRoot = convId === tweet.id;
   const text = tweet.note_tweet?.text || tweet.text;
-  const isThreadOpener = /(?:1\/(?:\d+|x|\s)|🧵|\bthread\b|⬇️|🔽|⤵️|↴|👇)/i.test(text);
+  const isThreadOpener = /(?:[01]\/(?:\d+|x|\s)|🧵|\bthread\b|⬇️|🔽|⤵️|↴|👇)/i.test(text);
 
   if (threadBuffers.has(convId)) {
     const buf = threadBuffers.get(convId);
