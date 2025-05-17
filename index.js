f9dfe88d5319b9186ccee27cd5f21a72dde660ad
diegodubai <dirkt82@hotmail.com>
2025-05-18 03:41:46 +0400

    Update index.js
    .replace(/ {2,}/g, ' ');
    
    Turns any succession of multiple spaces into 1 space max.

--- index.js
+++ index.js
@@ -158,11 +158,12 @@ function getFullTweetText(tweet, includes) {
     }
   }
 
-  // Removes http/https links and pic.x.com links along with preceding spaces, replaces new line with space, and replaces &amp; with &
+  // Removes http/https links and pic.x.com links along with preceding spaces, replaces new line with space, replaces &amp; with &, and finally turns any succession of multiple spaces into 1 space max.
   return text
     .replace(/ ?(?:https?:\/\/\S+|pic\.x\.com\/\S+)/g, '')
     .replace(/\n/g, ' ')
-    .replace(/&amp;/g, '&');
+    .replace(/&amp;/g, '&')
+    .replace(/ {2,}/g, ' ');
 }
 
 async function forwardTweet(tweet, includes) {
